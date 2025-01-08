bf02b80e1e6974a6f68dcf3dd24087c02e170d6d
meloddik <jboddie29@gmail.com>
2025-01-07 22:02:39 -0500

    Add update parameter for package updates

--- site-modules/profile/manifests/apt.pp
+++ site-modules/profile/manifests/apt.pp
@@ -1,6 +1,10 @@
 # Configure apt resources
 class profile::apt {
   class { 'apt':
+    update  => {
+      frequency => 'daily',
+      loglevel  => 'info',
+    },
     sources => lookup('sources', Hash, 'hash'),
   }
 }
